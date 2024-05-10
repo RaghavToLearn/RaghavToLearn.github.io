3fcdb67318c944133f301accde034024da4a96d5
RaghavToLearn <raghavendra.blog1@gmail.com>
2024-05-10 09:53:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,45 +6,87 @@ function showRates() {
   switch(selectedCountry) {
     case "US":
       ratesDetailsDiv.innerHTML = "<h3>United States</h3>" +
-                       "<p>Monthly Charge: $950 - $1,360</p>" +
+                       "<p>Monthly Charge: $950 - $1,360 (Based on complexity)</p>" +
+                       "<p>Base Charge: $950 </p>"+
                        "<p>Currency: USD</p>" +
                        "<p>Working Hours: 2 hours per day, 5 days a week</p>" +
-                       "<p>Payment Schedule: $475 - $680 every 15 days</p>";
+                       "<p>Payment Schedule: Half payment every 15 days</p>" +
+                       "<div class='rate-info'>" +
+                       "<h4>Payment Mode:</h4>" +
+                       "<p>PhonePay / Gpay</p>" +
+                       "<p>UPI number: +91-8328138429</p>" +
+                       "<p>UPI Holder name: Patlolla Raghavendra Reddy</p>" +
+                       "</div>";
       break;
     case "UK":
       ratesDetailsDiv.innerHTML = "<h3>United Kingdom</h3>" +
-                       "<p>Monthly Charge: £680 - £970</p>" +
+                       "<p>Monthly Charge: £900 - £1170 (Based on complexity)</p>" +
+                       "<p>Base Charge: £900 </p>"+
                        "<p>Currency: GBP</p>" +
                        "<p>Working Hours: 2 hours per day, 5 days a week</p>" +
-                       "<p>Payment Schedule: £340 - £485 every 15 days</p>";
+                       "<p>Payment Schedule: Half payment every 15 days</p>" +
+                       "<div class='rate-info'>" +
+                       "<h4>Payment Mode:</h4>" +
+                       "<p>PhonePay / Gpay</p>" +
+                       "<p>UPI number: +91-8328138429</p>" +
+                       "<p>UPI Holder name: Patlolla Raghavendra Reddy</p>" +
+                       "</div>";
       break;
     case "Canada":
       ratesDetailsDiv.innerHTML = "<h3>Canada</h3>" +
-                       "<p>Monthly Charge: $1,150 - $1,650</p>" +
+                       "<p>Monthly Charge: $1,250 - $1,650 (Based on complexity)</p>" +
+                       "<p>Base Charge: $1,250 </p>"+
                        "<p>Currency: CAD</p>" +
                        "<p>Working Hours: 2 hours per day, 5 days a week</p>" +
-                       "<p>Payment Schedule: $575 - $825 every 15 days</p>";
+                       "<p>Payment Schedule: Half payment every 15 days</p>" +
+                       "<div class='rate-info'>" +
+                       "<h4>Payment Mode:</h4>" +
+                       "<p>PhonePay / Gpay</p>" +
+                       "<p>UPI number: +91-8328138429</p>" +
+                       "<p>UPI Holder name: Patlolla Raghavendra Reddy</p>" +
+                       "</div>";
       break;
     case "EU":
       ratesDetailsDiv.innerHTML = "<h3>European Union</h3>" +
-                       "<p>Monthly Charge: €795 - €1,140</p>" +
+                       "<p>Monthly Charge: €900 - €1,140 (Based on complexity)</p>" +
+                       "<p>Base Charge: €900 </p>"+
                        "<p>Currency: EUR</p>" +
                        "<p>Working Hours: 2 hours per day, 5 days a week</p>" +
-                       "<p>Payment Schedule: €398 - €570 every 15 days</p>";
+                       "<p>Payment Schedule: Half payment every 15 days</p>" +
+                       "<div class='rate-info'>" +
+                       "<h4>Payment Mode:</h4>" +
+                       "<p>PhonePay / Gpay</p>" +
+                       "<p>UPI number: +91-8328138429</p>" +
+                       "<p>UPI Holder name: Patlolla Raghavendra Reddy</p>" +
+                       "</div>";
       break;
     case "SouthAfrica":
       ratesDetailsDiv.innerHTML = "<h3>South Africa</h3>" +
-                       "<p>Monthly Charge: R16,000 - R22,900</p>" +
+                       "<p>Monthly Charge: R12,000 - R15,900 (Based on complexity)</p>" +
+                       "<p>Base Charge: R12,000 </p>"+
                        "<p>Currency: ZAR</p>" +
                        "<p>Working Hours: 2 hours per day, 5 days a week</p>" +
-                       "<p>Payment Schedule: R8,000 - R11,450 every 15 days</p>";
+                       "<p>Payment Schedule: Half payment every 15 days</p>" +
+                       "<div class='rate-info'>" +
+                       "<h4>Payment Mode:</h4>" +
+                       "<p>PhonePay / Gpay</p>" +
+                       "<p>UPI number: +91-8328138429</p>" +
+                       "<p>UPI Holder name: Patlolla Raghavendra Reddy</p>" +
+                       "</div>";
       break;
     case "Nigeria":
       ratesDetailsDiv.innerHTML = "<h3>Nigeria</h3>" +
-                       "<p>Monthly Charge: ₦315,000 - ₦450,000</p>" +
+                       "<p>Monthly Charge: ₦900,000 - ₦1,200,000 (Based on complexity)</p>" +
+                       "<p>Base Charge: ₦900,000 </p>"+
                        "<p>Currency: NGN</p>" +
                        "<p>Working Hours: 2 hours per day, 5 days a week</p>" +
-                       "<p>Payment Schedule: ₦157,500 - ₦225,000 every 15 days</p>";
+                       "<p>Payment Schedule: Half payment every 15 days</p>" +
+                       "<div class='rate-info'>" +
+                       "<h4>Payment Mode:</h4>" +
+                       "<p>PhonePay / Gpay</p>" +
+                       "<p>UPI number: +91-8328138429</p>" +
+                       "<p>UPI Holder name: Patlolla Raghavendra Reddy</p>" +
+                       "</div>";
       break;
     default:
       ratesDetailsDiv.innerHTML = "<p>Please select a country to view rates.</p>";
